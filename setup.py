bcdee5288b8679caab4d1aa6f65a59aef4f19b80
Hamed Ahmadi <hamed@zerocater.com>
2016-12-08 13:46:09 -0800

    Adding pyjwt

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ def get_packages(package):
 
 setup(
     name='zc_events',
-    version='0.1.1',
+    version='0.1.2',
     description="Shared code for ZeroCater microservices events",
     long_description='',
     keywords='zerocater python util',
     author='ZeroCater',
     author_email='tech@zerocater.com',
     url='https://github.com/ZeroCater/zc_events',
-    download_url='https://github.com/ZeroCater/zc_events/tarball/0.1.1',
+    download_url='https://github.com/ZeroCater/zc_events/tarball/0.1.2',
     license='MIT',
     packages=get_packages('zc_events'),
     classifiers=[
@@ -40,5 +40,6 @@ setup(
         'redis>=2.10.5,<2.11.0',
         'ujson>=1.35,<1.36',
         'zc_common>=0.3.0',
+        'pyjwt>=1.4.0,<2.0.0',
     ]
 )
