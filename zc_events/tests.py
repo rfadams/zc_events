b0c746a732ecdfa0b1da64e7bcd5b702804841b0
Theophile Nsengimana <theo@zerocater.com>
2016-12-13 15:51:09 -0800

    Refactor GlobalIndexRebuildTestMixin

--- zc_events/tests.py
+++ zc_events/tests.py
@@ -1,6 +1,5 @@
 from __future__ import division
 
-import mock
 import uuid
 import math
 
@@ -10,6 +9,10 @@ from zc_events.utils import event_payload
 
 
 class GlobalIndexRebuildTestMixin(object):
+    """This is a base test case for all index rebuilding events.
+    See in legacy (zerocater.tasks.emit_event_tasks.index_data_events.py module) how to use it.
+    """
+
     index_rebuild_event_task = None
     resource_index_rebuild_task = None
     model = None
@@ -32,30 +35,27 @@ class GlobalIndexRebuildTestMixin(object):
     def create_test_data(self):
         raise NotImplementedError("Override this method to create test data")
 
-    @mock.patch('zc_common.events.emit.emit_microservice_event')
-    @mock.patch('zc_common.events.emit.save_to_s3file')
-    def test_emitting_event_with_default_batch_size__pass(self, mock_save_to_s3file, mock_emit_microservice_event):
+    def test_emitting_event_with_default_batch_size__pass(self, mock_save_string_contents_to_s3,
+                                                          mock_emit_microservice_event):
         self.resource_index_rebuild_task()
 
         events_count = int(math.ceil(self.objects_count / self.default_batch_size))
-        self.assertEqual(mock_save_to_s3file.call_count, events_count)
+        self.assertEqual(mock_save_string_contents_to_s3.call_count, events_count)
         self.assertEqual(mock_emit_microservice_event.call_count, events_count)
 
-    @mock.patch('zc_common.events.emit.emit_microservice_event')
-    @mock.patch('zc_common.events.emit.save_to_s3file')
-    def test_emitting_event_with_custom_batch_size__pass(self, mock_save_to_s3file, mock_emit_microservice_event):
+    def test_emitting_event_with_custom_batch_size__pass(self, mock_save_string_contents_to_s3,
+                                                         mock_emit_microservice_event):
         self.resource_index_rebuild_task(batch_size=self.custom_batch_size)
 
         events_count = int(math.ceil(self.objects_count / self.custom_batch_size))
-        self.assertEqual(mock_save_to_s3file.call_count, events_count)
+        self.assertEqual(mock_save_string_contents_to_s3.call_count, events_count)
         self.assertEqual(mock_emit_microservice_event.call_count, events_count)
 
-    @mock.patch('zc_common.events.emit.emit_microservice_event')
-    @mock.patch('zc_common.events.emit.save_to_s3file')
-    def test_emitting_event_with_correct_attributes__pass(self, mock_save_to_s3file, mock_emit_microservice_event):
+    def test_emitting_event_with_correct_attributes__pass(self, mock_save_string_contents_to_s3,
+                                                          mock_emit_microservice_event):
         total_events = int(math.ceil(self.objects_count / self.default_batch_size))
         s3_keys = [str(uuid.uuid4()) for i in xrange(total_events)]
-        mock_save_to_s3file.side_effect = s3_keys
+        mock_save_string_contents_to_s3.side_effect = s3_keys
 
         self.resource_index_rebuild_task()
 
@@ -75,16 +75,14 @@ class GlobalIndexRebuildTestMixin(object):
 
                 data.append(instance_data)
 
-            mock_save_to_s3file.assert_any_call(data, settings.AWS_INDEXER_BUCKET_NAME)
+            mock_save_string_contents_to_s3.assert_any_call(data, settings.AWS_INDEXER_BUCKET_NAME)
 
             s3_key = s3_keys[events_count]
             payload = event_payload(self.resource_type, None, None, {'s3_key': s3_key})
             mock_emit_microservice_event.assert_any_call(self.event_name, **payload)
             events_count += 1
 
-    @mock.patch('zc_common.events.emit.emit_microservice_event')
-    @mock.patch('zc_common.events.emit.save_to_s3file')
-    def test_filtering_data_works__pass(self, mock_save_to_s3file, mock_emit_microservice_event):
+    def test_filtering_data_works__pass(self, mock_save_string_contents_to_s3, mock_emit_microservice_event):
         # If both model and queryset are defined, this test ensures filters applied on the queryset returns less
         # objects than using model.objects.all()
         if self.queryset and self.model:
@@ -93,13 +91,11 @@ class GlobalIndexRebuildTestMixin(object):
                   'Otherwise, set queryset to None.'
             self.assertTrue(self.queryset.count() < self.model.objects.count(), msg)
 
-    @mock.patch('zc_common.events.emit.emit_microservice_event')
-    @mock.patch('zc_common.events.emit.save_to_s3file')
     def test_event_not_emitted_from_received_index_rebuild_event_with_different_resource_type__pass(
-            self, mock_save_to_s3file, mock_emit_microservice_event):
+            self, mock_save_string_contents_to_s3, mock_emit_microservice_event):
 
         self.index_rebuild_event_task(resource_type='Some Other Undefined Resource Type',
                                       meta={'batch_size': self.custom_batch_size})
 
-        mock_save_to_s3file.assert_not_called()
+        mock_save_string_contents_to_s3.assert_not_called()
         mock_emit_microservice_event.assert_not_called()
