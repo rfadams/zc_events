21a4d5b002ecfcfb1b7dc43e9b0f1495c619f0b0
Hamed Ahmadi <hamed@zerocater.com>
2017-05-15 13:39:32 -0700

    Support email unsubscribe group

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ def get_packages(package):
 
 setup(
     name='zc_events',
-    version='0.1.9',
+    version='0.1.10b',
     description="Shared code for ZeroCater microservices events",
     long_description='',
     keywords='zerocater python util',
     author='ZeroCater',
     author_email='tech@zerocater.com',
     url='https://github.com/ZeroCater/zc_events',
-    download_url='https://github.com/ZeroCater/zc_events/tarball/0.1.9',
+    download_url='https://github.com/ZeroCater/zc_events/tarball/0.1.10b',
     license='MIT',
     packages=get_packages('zc_events'),
     classifiers=[
